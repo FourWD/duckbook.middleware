45c06778dafcdc5c5085cb8845a253cfb9cbbe45
Plew Woo <pew_woothiphom@hotmail.com>
2024-05-28 14:30:22 +0700

    Create vehicle-scraping-log.go

---
+++ orm/vehicle-scraping-log.go
@@ -0,0 +1,21 @@
+package orm
+
+import "github.com/FourWD/middleware/orm"
+
+type VehicleScrapingLog struct {
+	orm.Vehicle
+	Name              string
+	Web               string
+	SKU               string
+	Brand             string
+	Model             string
+	Gear              string
+	Province          string
+	Dealer            string
+	Remark            string
+	Mile              string
+	YearManufacturing string
+	PricePreVat       string
+	Price             string
+	BodyType          string
+}
