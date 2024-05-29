c123f433f19f861843db07c9ae2b5c3807020cb5
Plew Woo <pew_woothiphom@hotmail.com>
2024-05-29 11:43:24 +0700

    Update ORM

--- orm/web.go
+++ orm/web.go
@@ -5,6 +5,7 @@ import "github.com/FourWD/middleware/model"
 type Web struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
 	model.GormModel
-	Web     string `json:"web" query:"web" gorm:"type:varchar(256);"`
-	MaxPage int    `json:"max_page" query:"max_page" gorm:"type:int;"`
+	Web       string `json:"web" query:"web" gorm:"type:varchar(256);"`
+	MaxPage   int    `json:"max_page" query:"max_page" gorm:"type:int;"`
+	SleepHour int    `json:"sleep_hour" query:"sleep_hour" gorm:"type:int;"`
 }
