835cd1aa867c7f1a010d10f1a8e27ee9f657fdc2
Plew Woo <pew_woothiphom@hotmail.com>
2024-05-29 11:30:50 +0700

    Update log-vehicle-scraping.go

--- orm/log-vehicle-scraping.go
+++ orm/log-vehicle-scraping.go
@@ -2,5 +2,5 @@ package orm
 
 type LogVehicleScraping struct {
 	VehicleScraping
-	OriginalID string `json:"original_id" query:"original_id" gorm:"type:varchar(256),index"`
+	OriginalID string `json:"original_id" query:"original_id" gorm:"type:varchar(256),index:idx_original_id"`
 }
