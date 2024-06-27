8b929ae64d5429ba111e02e0f0a0de3407a0d731
Plew Woo <pew_woothiphom@hotmail.com>
2024-06-27 17:43:46 +0700

    Update web.go

--- orm/web.go
+++ orm/web.go
@@ -8,4 +8,5 @@ type Web struct {
 	Web       string `json:"web" query:"web" gorm:"type:varchar(256);"`
 	MaxPage   int    `json:"max_page" query:"max_page" gorm:"type:int;"`
 	SleepHour int    `json:"sleep_hour" query:"sleep_hour" gorm:"type:int;"`
+	Color     string `json:"color" query:"color" gorm:"type:varchar(7);"`
 }
